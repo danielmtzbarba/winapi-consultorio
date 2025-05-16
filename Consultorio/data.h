c27508acb03beb6cc8a02083a42f0d7146cd583d
danielmtz <danielmtz.dev@gmail.com>
2025-05-16 08:48:01 -0600

    Cleaning project.

--- Consultorio/data.h
+++ Consultorio/data.h
@@ -1,6 +1,15 @@
 #pragma once
-#include <windows.h>
+
+#include "users.h"
+#include "resources.h"
 
 extern HINSTANCE hInst;
 
 extern INT_PTR CALLBACK WindowProcLogin(HWND, UINT, WPARAM, LPARAM);
+extern INT_PTR CALLBACK WindowProcSignUp(HWND, UINT, WPARAM, LPARAM);
+extern INT_PTR CALLBACK WindowProcMenu(HWND, UINT, WPARAM, LPARAM);
+//extern INT_PTR CALLBACK WindowProcMedic(HWND, UINT, WPARAM, LPARAM);
+//extern INT_PTR CALLBACK WindowProcPatient(HWND, UINT, WPARAM, LPARAM);
+
+
+
