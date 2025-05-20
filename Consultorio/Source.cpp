597edb8f32a3ca7a00eb5bb0f5b3f8ee94072494
danielmtz <danielmtz.dev@gmail.com>
2025-05-20 08:20:58 -0600

    test

--- Consultorio/Source.cpp
+++ Consultorio/Source.cpp
@@ -1,6 +1,7 @@
-#pragma once
-
 #include "data.h"
+
+#include <commctrl.h>
+#pragma comment(lib, "comctl32.lib")
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     AppData::Instance().hInst = hInstance;
