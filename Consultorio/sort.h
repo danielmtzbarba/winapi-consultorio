77ce165dc4583fa5d94961862ef029c0764cfee7
danielmtz <danielmtz.dev@gmail.com>
2025-05-20 14:54:44 -0600

    heap sort

--- Consultorio/sort.h
+++ Consultorio/sort.h
@@ -38,17 +38,64 @@ void quickSort(NodeType* low, NodeType* high, Compare comp, SwapFunc swapData) {
     }
 }
 
+
 // GENERIC **HEAP SORT** FOR **DOUBLYLINKEDLIST**
+template<typename NodeType, typename Compare, typename SwapFunc>
+void heapify(NodeType* arr[], int n, int i, Compare comp, SwapFunc swapData) {
+    int largest = i;      // Assume root is largest
+    int left = 2 * i + 1; // Left child
+    int right = 2 * i + 2;// Right child
 
+    if (left < n && comp(arr[largest], arr[left]))
+        largest = left;
 
+    if (right < n && comp(arr[largest], arr[right]))
+        largest = right;
 
+    if (largest != i) {
+        swapData(arr[i], arr[largest]);
+        heapify(arr, n, largest, comp, swapData);
+    }
+}
 
+template<typename NodeType, typename Compare, typename SwapFunc>
+void heapSort(NodeType* head, NodeType* tail, Compare comp, SwapFunc swapData) {
+    // Convert linked list to array of pointers
+    std::vector<NodeType*> nodes;
+    for (NodeType* curr = head; curr != nullptr; curr = curr->next) {
+        nodes.push_back(curr);
+    }
 
+    int n = nodes.size();
 
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(nodes.data(), n, i, comp, swapData);
+    }
+
+    // Extract elements from heap
+    for (int i = n - 1; i >= 0; i--) {
+        swapData(nodes[0], nodes[i]);
+        heapify(nodes.data(), i, 0, comp, swapData);
+    }
+}
 
 // SEARCH FUNCTIONS FOR PROGRAM
+
+// QUICKSORT
 inline void sortByDate() {
     quickSort<AppointmentNode>(
+        AppData::Instance().app_list.head,
+        AppData::Instance().app_list.tail,
+        [](AppointmentNode* a, AppointmentNode* b) {
+            return dateStrToIntTuple(a->date) < dateStrToIntTuple(b->date);
+        },
+        AppData::Instance().app_list.swapData
+    );
+}
+// HEAPSORT
+inline void sortAppointmentsByDateHeap() {
+    heapSort<AppointmentNode>(
         AppData::Instance().app_list.head,
         AppData::Instance().app_list.tail,
         [](AppointmentNode* a, AppointmentNode* b) {
